2288e4f6bef51cf66234bc7af47185972fc6aeb5
Yuki Urakawa <urakawa.0205@gmail.com>
2016-09-18 02:09:31 +0900

    Fix symbolic link path

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
-ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
-sudo ln -sf ~/dotfiles/bin/wifi /usr/local/bin/wifi && sudo chmod +x /usr/local/bin/wifi
-sudo ln -sf ~/dotfiles/bin/battery /usr/local/bin/battery && sudo chmod +x /usr/local/bin/battery
+ln -sf $PWD/.zshrc ~/.zshrc
+ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/.gvimrc ~/.gvimrc
+ln -sf $PWD/.ideavimrc ~/.ideavimrc
+ln -sf $PWD/.xvimrc ~/.xvimrc
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
+ln -sf $PWD/.gitconfig ~/.gitconfig
+
+sudo ln -sf $PWD/bin/wifi /usr/local/bin/wifi && sudo chmod +x /usr/local/bin/wifi
+sudo ln -sf $PWD/bin/battery /usr/local/bin/battery && sudo chmod +x /usr/local/bin/battery
