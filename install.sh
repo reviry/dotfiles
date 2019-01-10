f115cf4ef227a9e594d0c8c77963ab44845f7316
Yuki Urakawa <urakawa.0205@gmail.com>
2019-01-10 15:32:42 +0900

    symlink

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ ln -sf $PWD/.gitconfig ~/.gitconfig
 ln -sf $PWD/.gitignore_global ~/.gitignore_global
 ln -sf $PWD/vim/dein.toml ~/.vim/rc/dein.toml
 ln -sf $PWD/vim/deinlazy.toml ~/.vim/rc/deinlazy.toml
+ln -sf $PWD/.vimrc ~/.config/nvim/init.vim
 
 sudo ln -sf $PWD/bin/wifi /usr/local/bin/wifi && sudo chmod +x /usr/local/bin/wifi
 sudo ln -sf $PWD/bin/battery /usr/local/bin/battery && sudo chmod +x /usr/local/bin/battery
